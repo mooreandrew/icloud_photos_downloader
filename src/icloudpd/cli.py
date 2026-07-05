684ba561e11b7cd058c080e006f6fbbe30b7f345
Andrew Moore <andrewmoore@Andrews-MacBook-Air.local>
2026-07-05 12:20:44 +0100

    Fixed missing variable

--- src/icloudpd/cli.py
+++ src/icloudpd/cli.py
@@ -133,7 +133,7 @@ def add_options_for_user(parser: argparse.ArgumentParser) -> argparse.ArgumentPa
     cloned.add_argument(
         "--folder-structure",
         help="Folder structure. If set to `none`, all photos will be placed into the download directory. Default: %(default)s",
-        default="{:%Y%m - %B %Y}",
+        default="{:%Y/%m/%d}",
         type=validate_folder_structure,
     )
     cloned.add_argument(
@@ -256,13 +256,6 @@ def add_options_for_user(parser: argparse.ArgumentParser) -> argparse.ArgumentPa
         help="Don't download any photos (default: download all photos and videos)",
         action="store_true",
     )
-    cloned.add_argument(
-        "--download-suffix",
-        help="Set the suffix of the download file Default: empty",
-        default='',
-        type=str,
-    )
-
     return cloned
 
 
