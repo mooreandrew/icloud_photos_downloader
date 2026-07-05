0de45d9a756abe6f173eb60ca069f57eb9f489d9
Andrew Moore <andrewmoore@Andrews-MacBook-Air.local>
2026-07-05 12:26:40 +0100

    Fixed missing variable

--- src/icloudpd/config.py
+++ src/icloudpd/config.py
@@ -50,6 +50,7 @@ class _DefaultConfig:
     skip_created_before: datetime.datetime | datetime.timedelta | None
     skip_created_after: datetime.datetime | datetime.timedelta | None
     skip_photos: bool
+    download_suffix: str
 
 
 @dataclass(kw_only=True)
